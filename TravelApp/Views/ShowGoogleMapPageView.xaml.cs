b4651b9d8c1d09919ce94cfbdacc6028ab277791
Enkeli __ <programmistik@hotmail.com>
2019-01-11 11:46:49 +0400

    --

--- TravelApp/Views/ShowGoogleMapPageView.xaml.cs
+++ TravelApp/Views/ShowGoogleMapPageView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,23 @@ namespace TravelApp.Views
             InitializeComponent();
         }
 
-        
+        //private void AllpyMapType(object sender, RoutedEventArgs e)
+        //{
+        //    var MapUri = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\map1.html";
+        //    var NewMapUri = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\map2.html";
+        //    if (File.Exists(MapUri))
+        //    {
+        //        StreamReader objReader = new StreamReader(MapUri);
+        //        string line = "";
+        //        line = objReader.ReadToEnd();
+        //        objReader.Close();
+        //        var Itm = (ComboBoxItem)MapType.SelectedItem;
+        //        line = line.Replace("roadmap", Itm.Content.ToString());
+        //        StreamWriter page = File.CreateText(NewMapUri);
+        //        page.Write(line);
+        //        page.Close();
+        //        myWeb.Navigate(NewMapUri);
+        //    }
+        //}
     }
 }
